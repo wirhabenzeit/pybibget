6a4c497753b5e17e49a4db047da1d4a27859108f
Dominik Schröder <dschroeder@ethz.ch>
2023-01-05 15:21:13 +0100

    Added code comments

--- pybibget/__init__.py
+++ pybibget/__init__.py
@@ -1,13 +1,21 @@
-import argparse, re
-import pybibget.bibentry as bibentry
+import argparse
+import re
+import sys
 import pybtex.database
+import pybibget.bibentry as bibentry
 
-def pybibget():
-    parser = argparse.ArgumentParser(prog ='pybibget',description ='Command line utility to automatically retrieve BibTeX citations from MathSciNet, arXiv and PubMed')
-  
-    parser.add_argument('keys', type = str, metavar ='citekeys', nargs='*',help ='MathSciNet (MRxxxxx), arXiv (xxxx.xxxxx), PubMed (PMID:xxxxxxxx) or DOI (10.xxx/xxxxx) citation keys (separated by spaces)')
+def pybibget() -> None:
+    """
+    Reads citation keys from command line and calls get_citations()
+    """
+    parser = argparse.ArgumentParser(prog = 'pybibget', description = 'Command line utility to automatically retrieve BibTeX citations from MathSciNet, arXiv and PubMed')
+
+    parser.add_argument('keys', type = str, metavar = 'citekeys', nargs='*',
+    help ='MathSciNet (MRxxxxx), arXiv (xxxx.xxxxx), PubMed (PMID:xxxxxxxx)'\
+    'or DOI (10.xxx/xxxxx) citation keys (separated by spaces)')
     parser.add_argument('-v','--verbose',action='store_true',help='verbose output')
-    parser.add_argument('-f', action='store',dest='file_name',help='Append output to file (default: write output to stdout)')
+    parser.add_argument('-f', action='store',dest='file_name',
+    help='Append output to file (default: write output to stdout)')
 
     args = parser.parse_args()
     if not args.keys:
@@ -16,48 +24,64 @@ def pybibget():
     get_citations(args.keys,verbose=args.verbose,file=args.file_name)
 
 def pybibparse():
-    parser = argparse.ArgumentParser(prog ='pybibget',description ='Command line utility to automatically retrieve missing BibTeX citations from MathSciNet, arXiv and PubMed')
-  
-    parser.add_argument('file_name', type = str, metavar ='file name', nargs=1, help ='base file name of main tex file (without .tex extension)')
+    """
+    Reads latex file name from the command line, parses the .blg file and calls get_citations()
+    """
+    parser = argparse.ArgumentParser(prog ='pybibget',
+    description ='Command line utility to automatically retrieve missing BibTeX citations'\
+        'from MathSciNet, arXiv and PubMed')
+
+    parser.add_argument('file_name', type = str, metavar ='file name', nargs=1, 
+    help ='base file name of main tex file (without .tex extension)')
     parser.add_argument('-v','--verbose',action='store_true',help='verbose output')
-    parser.add_argument('-w','--write',metavar="file_name",action='store', nargs='?', const=" ", type=str,help='Append output to .bib file (default: write output to stdout). The .bib file is automatically detected from the .blg file. If no .bib file is found, the .bib file has to be specified explicitly via "-w file_name.bib".')
+    parser.add_argument('-w','--write', metavar="file_name",action='store', nargs='?', const=" ", 
+    type=str, help='Append output to .bib file (default: write output to stdout). The .bib '\
+        'file is automatically detected from the .blg file. If no .bib file is found, '\
+        'the .bib file has to be specified explicitly via "-w file_name.bib".')
 
     args = parser.parse_args()
     if not args.file_name:
         parser.print_help()
-        exit(1)
+        sys.exit()
 
-    baseFileName = args.file_name[0]
-    blgFile = open(baseFileName+".blg")
-    blgFile = blgFile.read()
-    missingCites = re.findall(r"I didn't find a database entry for '([A-Za-z0-9\.\-_ :\/]*)'", blgFile) + re.findall(r'I didn\'t find a database entry for "([A-Za-z0-9\.\-_ :\/]*)"', blgFile)
-    bibFileNames = re.findall(r"Found BibTeX data source '([A-Za-z0-9.\-_\/]*)'", blgFile) + re.findall(r"Looking for bibtex file '([A-Za-z0-9.\-_\/]*)'", blgFile) + re.findall(r'Database file #\d: ([A-Za-z0-9.\-_\/]*)\n', blgFile) + re.findall(r'I couldn\'t open database file ([A-Za-z0-9.\-_\/]*)\n', blgFile)
+    base_file_name = args.file_name[0]
+    with open(base_file_name+".blg") as f:
+        blg_file = f.read()
+    missing_cites = re.findall(r"I didn't find a database entry for '([A-Za-z0-9\.\-_ :\/]*)'", blg_file) + re.findall(r'I didn\'t find a database entry for "([A-Za-z0-9\.\-_ :\/]*)"', blg_file)
+    bib_file_names = re.findall(r"Found BibTeX data source '([A-Za-z0-9.\-_\/]*)'", blg_file) \
+    + re.findall(r"Looking for bibtex file '([A-Za-z0-9.\-_\/]*)'", blg_file) \
+    + re.findall(r'Database file #\d: ([A-Za-z0-9.\-_\/]*)\n', blg_file) \
+    + re.findall(r'I couldn\'t open database file ([A-Za-z0-9.\-_\/]*)\n', blg_file)
 
-    if missingCites:
+    if missing_cites:
         if args.write is None:
-            get_citations(missingCites,verbose=args.verbose)
+            get_citations(missing_cites,verbose=args.verbose)
         elif args.write != " ":
-            get_citations(missingCites,verbose=args.verbose,file=args.write)
-        elif args.write == " " and bibFileNames:
-            get_citations(missingCites,verbose=args.verbose,file=bibFileNames[0])
+            get_citations(missing_cites,verbose=args.verbose,file=args.write)
+        elif args.write == " " and bib_file_names:
+            get_citations(missing_cites,verbose=args.verbose,file=bib_file_names[0])
         else:
-            print("No .bib file found. Please specify the .bib file explicitly via '-w file_name.bib'")
+            print("No .bib file found. Please specify the .bib file via '-w file_name.bib'")
     else:
-        print("No missing citations found. Make sure that biber/bibtex is run successfully before running pybibget.")
-    
+        print("No missing citations found. Make sure that biber/bibtex is run '\
+            'successfully before running pybibget.")
+
 def get_citations(keys,verbose=False,file=None):
+    """
+    Retrieves BibTeX entries for given citation keys and writes them to file or stdout
+    """
     bib_data = pybtex.database.BibliographyData()
     for key in keys:
         try:
             bib_data.entries[key] = bibentry.getbibentry(key,verbose=verbose)
-        except ValueError as e:
-            print(e)
+        except ValueError as error:
+            print(error)
     number_of_entries = len(bib_data.entries)
     bib_data = bib_data.to_string('bibtex')
     if file:
         with open(file,'a') as f:
             f.write(bib_data)
-            print(f"Succesfully appended {number_of_entries} BibTeX entries to {file}.")
+            print(f"Successfully appended {number_of_entries} BibTeX entries to {file}.")
     else:
         if verbose:
             print("\n======================\n Output:\n======================")
--- pybibget/bibentry.py
+++ pybibget/bibentry.py
@@ -1,114 +1,218 @@
+from urllib import parse
+import re
 from lxml import html, etree
-import requests,re,json
+import requests
 from pybtex.database import Entry, Person, parse_string, parse_bytes
 from pylatexenc.latexencode import unicode_to_latex
 from pylatexenc.latex2text import LatexNodes2Text
-from urllib import parse
-atom_namespace = 'http://www.w3.org/2005/Atom'
-arxiv_namespace = 'http://arxiv.org/schemas/atom'
-re_mathscinet = r'MR\d{4,10}'
-re_pubmed = r'PMID:\d{4,10}'
-re_doi = r'10\.\d{4,9}\/[-._;()/:A-Za-z0-9]+'
-re_arxiv_old = r'\b[a-zA-Z\-\.]{2,10}\/\d{7}(?:v\d)?\b'
-re_arxiv_new = r'\b\d{4}\.\d{4,5}(?:v\d)?\b'
+ATOM = 'http://www.w3.org/2005/Atom'
+ARXIV = 'http://arxiv.org/schemas/atom'
+RE_MSC = r'MR\d{4,10}'
+RE_PMID = r'PMID:\d{4,10}'
+RE_DOI = r'10\.\d{4,9}\/[-._;()/:A-Za-z0-9]+'
+RE_ARXIV_OLD = r'\b[a-zA-Z\-\.]{2,10}\/\d{7}(?:v\d)?\b'
+RE_ARXIV_NEW = r'\b\d{4}\.\d{4,5}(?:v\d)?\b'
 
-def getbibentry(id,verbose=False):
-    if re.match(re_mathscinet,id):
+def getbibentry(key,verbose=False):
+    """
+    Get a bibentry from a citation key.
+
+    Parameters
+    ----------
+    key : str
+        The citation key.
+    verbose : bool, optional
+        Whether to print the progress of the function. The default is False.
+
+    Returns
+    ---------
+    bibentry : pybtex.database.Entry
+
+    Raises
+    ----------
+    ValueError
+        If the citation key is invalid or the entry is not found.
+    """
+    if re.match(RE_MSC,key):
         if verbose:
-            print(f"Looking for {id} on MathSciNet...",end=" ")
-        return get_mathscinet_bibentry(id=id,verbose=verbose)
-    elif re.match(re_pubmed,id):
+            print(f"Looking for {key} on MathSciNet...",end=" ")
+        return get_mathscinet_bibentry(mrkey=key,verbose=verbose)
+    if re.match(RE_PMID,key):
         if verbose:
-            print(f"Looking for {id} on PubMed...",end=" ")
-        return get_pubmed_bibentry(id=id,verbose=verbose)
-    elif re.match(re_arxiv_old, id) or re.match(re_arxiv_new, id):
+            print(f"Looking for {key} on PubMed...",end=" ")
+        return get_pubmed_bibentry(pmid=key,verbose=verbose)
+    if re.match(RE_ARXIV_OLD, key) or re.match(RE_ARXIV_NEW, key):
         if verbose:
-            print(f"Looking for arXiv:{id}...",end=" ")
-        return get_arxiv_bibentry(id,verbose=verbose)
-    elif re.match(re_doi,id):
+            print(f"Looking for arXiv:{key}...",end=" ")
+        return get_arxiv_bibentry(key,verbose=verbose)
+    if re.match(RE_DOI,key):
         try:
             if verbose:
-                print(f"Looking for {id} on MathSciNet...",end=" ")
-            return get_mathscinet_bibentry(doi=id,verbose=verbose)
+                print(f"Looking for {key} on MathSciNet...",end=" ")
+            return get_mathscinet_bibentry(doi=key,verbose=verbose)
         except ValueError:
             if verbose:
-                print(f"Not found. Looking for {id} on doi.org...",end=" ")
-            return get_doi_bibentry(id,verbose=verbose)
+                print(f"Not found. Looking for {key} on doi.org...",end=" ")
+            return get_doi_bibentry(key,verbose=verbose)
     else:
-        raise ValueError(f"Invalid citation key {id}!\n")
+        raise ValueError(f"Invalid citation key {key}!\n")
 
-def get_mathscinet_bibentry(id=None,doi=None,verbose=False):
-    if id:
-        url = "https://mathscinet.ams.org/mathscinet/search/publications.html?fmt=bibtex&pg1=MR&s1=" + id[2:]
+def get_mathscinet_bibentry(mrkey=None,doi=None,verbose=False):
+    """
+    Get a bibentry from a MathSciNet citation key or DOI.
+
+    Parameters
+    ----------
+    mrkey : str, optional
+        The MathSciNet citation key. The default is None.
+    doi : str, optional
+        The DOI. The default is None.
+    verbose : bool, optional
+        Whether to print the progress of the function. The default is False.
+
+    Returns
+    ---------
+    bibentry : pybtex.database.Entry
+
+    Raises
+    ----------
+    ValueError
+        If the neither mrnumber nor doi are provided, or the entry is not found.
+    """
+    if mrkey:
+        url = "https://mathscinet.ams.org/mathscinet/search/publications.html?fmt=bibtex&pg1=MR&s1=" + mrkey[2:]
     elif doi:
         url = "https://mathscinet.ams.org/mathscinet/search/publications.html?fmt=bibtex&pg1=DOI&s1=" + doi
+    else:
+        raise ValueError("Either mrkey or doi must be specified.")
     if verbose:
         print(f"Connecting to {url}...",end=" ")
-    page = requests.get(url)
+    page = requests.get(url,timeout=10)
     tree = html.fromstring(page.content)
     if bibstrings := tree.xpath('//pre/text()'):
         if verbose:
             print("Success!")
         bibstr=bibstrings[0]
     else:
-        raise ValueError(f"{id if id else doi} not found! Please check the citation key and whether you have access to MathSciNet.")
+        raise ValueError(f"{mrkey if mrkey else doi} not found! Please check the citation key and whether you have access to MathSciNet.")
     entries = parse_string(bibstr,'bibtex').entries
     univ_id= list(entries.keys())[0]
     return entries[univ_id]
 
-def get_doi_bibentry(id,verbose=False):
-    url = "https://doi.org/" + id
+def get_doi_bibentry(doi,verbose=False):
+    """
+    Get a bibentry from a DOI.
+
+    Parameters
+    ----------
+    doi : str
+        The DOI.
+    verbose : bool, optional
+        Whether to print the progress of the function. The default is False.
+
+    Returns
+    ---------
+    bibentry : pybtex.database.Entry
+
+    Raises
+    ----------
+    ValueError
+        If the entry is not found.
+    """
+    url = "https://doi.org/" + doi
     headers = { 'Accept': 'application/x-bibtex; charset=utf-8' }
-    page = requests.get(url,headers=headers)
+    page = requests.get(url,headers=headers,timeout=10)
     if page.status_code == 200:
         entries = parse_bytes(page.content,'bibtex').entries
         if verbose:
             print("Success!")
         return sanitize_entry(list(entries.values())[0])
     else:
-        raise ValueError(f"Not found! Please check the citation key and whether you have access to doi.org.")
+        raise ValueError("Not found! Please check the citation key and whether you have access to doi.org.")
 
-def get_arxiv_bibentry(id,verbose=False):
-    url = "http://export.arxiv.org/api/query?id_list=" + id
-    page = requests.get(url)
+def get_arxiv_bibentry(arxiv_key,verbose=False):
+    """
+    Get a bibentry from an arXiv identifier.
+
+    Parameters
+    ----------
+    arxiv_key : str
+        The arXiv identifier.
+    verbose : bool, optional
+        Whether to print the progress of the function. The default is False.
+
+    Returns
+    ---------
+    bibentry : pybtex.database.Entry
+
+    Raises
+    ----------
+    ValueError
+        If the entry is not found.
+    """
+    url = "http://export.arxiv.org/api/query?id_list=" + arxiv_key
+    page = requests.get(url,timeout=10)
     tree = etree.fromstring(page.content)
-    if doi := tree.xpath("//a:entry/b:doi",namespaces={ 'a': atom_namespace,'b': arxiv_namespace }):
+    if doi := tree.xpath("//a:entry/b:doi",namespaces={ 'a': ATOM,'b': ARXIV }):
         if verbose:
             print("Detected DOI in arXiv record...",end=" ")
         bibentry = getbibentry(doi[0].text,verbose=verbose)
-    elif title := tree.xpath("//a:entry/a:title",namespaces={ 'a': atom_namespace }):
+    elif title := tree.xpath("//a:entry/a:title",namespaces={ 'a': ATOM }):
         if verbose:
             print("Success!")
         fields = [("title", title[0].text)]
-        if journal := tree.xpath("//a:entry/a:journal",namespaces={ 'a': atom_namespace }):
+        if journal := tree.xpath("//a:entry/a:journal",namespaces={ 'a': ATOM }):
             fields += [("note",journal[0].text)]
         else:
             fields += [("note","Preprint")]
-        fields += [("year", tree.xpath("//a:entry/a:published",namespaces={ 'a':atom_namespace })[0].text[:4])]
+        fields += [("year", tree.xpath("//a:entry/a:published",namespaces={ 'a':ATOM })[0].text[:4])]
         bibentry = Entry("unpublished", fields=fields)
-        bibentry.persons["author"] = [Person(author.text) for author in tree.xpath("//a:entry/a:author/a:name",namespaces={ 'a': atom_namespace })]
+        bibentry.persons["author"] = [Person(author.text) for author in tree.xpath("//a:entry/a:author/a:name",namespaces={ 'a': ATOM })]
         bibentry = sanitize_entry(bibentry)
     else:
-        raise ValueError(f"Could not find citation for {id}. Please check the citation key and whether you have access to arXiv.")
-    bibentry.fields["eprint"] = id
+        raise ValueError(f"Could not find citation for {arxiv_key}. Please check the citation key and whether you have access to arXiv.")
+    bibentry.fields["eprint"] = arxiv_key
     bibentry.fields["archiveprefix"] = "arXiv"
-    return bibentry 
- 
-def get_pubmed_bibentry(id,verbose=False):
-    r = requests.get(f'https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?ids={id[5:]}&idtype=pmid&format=json',headers={'Accept': 'application/json'})
-    if r.status_code == 200:
-        data = r.json()
+    return bibentry
+
+def get_pubmed_bibentry(pmid, verbose=False):
+    """
+    Get a bibentry from Pubmed for a given PubMed ID.
+
+    Parameters
+    ----------
+    pmid : str
+        The PubMed ID. 
+    verbose : bool, optional
+        Whether to print the progress of the function. The default is False.
+
+    Returns
+    ---------
+    bibentry : pybtex.database.Entry
+
+    Raises
+    ----------
+    ValueError
+        If the entry is not found.
+    """
+    url = f'https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?ids={pmid[5:]}&idtype=pmid&format=json'
+    req = requests.get(url, headers={'Accept': 'application/json'},timeout=10)
+    if req.status_code == 200:
+        data = req.json()
         try:
             doi = data['records'][0]['doi']
             if verbose:
                 print(f"Detected doi.org/{doi} in PubMed record...",end=" ")
             bibentry = sanitize_entry(getbibentry(doi,verbose=verbose))
-        except:
-            raise ValueError(f"Could not find citation for {id}. Please check the citation key and whether you have access to PubMed.")
-        bibentry.fields["PMID"] = id[5:]
+        except Exception as exc:
+            raise ValueError(f'Could not find citation for {pmid}. Please check the citation key and whether you have access to PubMed.') from exc
+        bibentry.fields["PMID"] = pmid[5:]
         return bibentry
 
 def sanitize_entry(entry):
+    """
+    Sanitize a bibentry. Protects title capitalization, removes newlines and tabs, and converts unicode characters to LaTeX.
+    """
     if "title" in entry.fields:
         entry.fields["title"] = sanitize_string(entry.fields["TITLE"],title=True)
     for author in entry.persons["AUTHOR"]:
@@ -121,6 +225,9 @@ def sanitize_entry(entry):
     return entry
 
 def sanitize_string(string,title=False):
+    """
+    Sanitize a string: Removes newlines and tabs, and converts unicode characters to LaTeX. If title is True, also protects title capitalization.
+    """
     string = string.replace("\n", "").replace("\t", "")
     string = LatexNodes2Text().latex_to_text(string)
     string = unicode_to_latex(string)
