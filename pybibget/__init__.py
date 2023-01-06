490ccccb3cfc5cd4ca6f18ed1a12bb99cb9d08bb
Dominik Schröder <dschroeder@ethz.ch>
2023-01-06 12:09:36 +0100

    Improved error handling and logging

--- pybibget/__init__.py
+++ pybibget/__init__.py
@@ -2,6 +2,8 @@ import argparse
 import asyncio
 import re
 import sys
+import logging as log
+from enum import Enum
 import pybtex.database
 import pybibget.bibentry as bibentry
 
@@ -14,13 +16,19 @@ def pybibget() -> None:
 
     parser.add_argument('keys', type=str, metavar='citekeys', nargs='*', help='MathSciNet (MRxxxxx), arXiv (xxxx.xxxxx), PubMed (PMID:xxxxxxxx) or DOI (10.xxx/xxxxx) citation keys (separated by spaces)')
     parser.add_argument('-v', '--verbose', action='store_true', help='verbose output')
+    parser.add_argument('-d', '--debug', action='store_true', help='debug output')
     parser.add_argument('-f', action='store', dest='file_name', help='Append output to file (default: write output to stdout)')
 
     args = parser.parse_args()
+    kwargs = {'file': args.file_name}
+    if args.debug:
+        kwargs['verbose'] = log.DEBUG
+    elif args.verbose:
+        kwargs['verbose'] = log.INFO
     if not args.keys:
         parser.print_help()
         exit(1)
-    asyncio.run(get_citations(args.keys, verbose=args.verbose, file=args.file_name))
+    asyncio.run(get_citations(args.keys, **kwargs))
 
 
 def pybibparse():
@@ -31,6 +39,7 @@ def pybibparse():
 
     parser.add_argument('file_name', type=str, metavar='file_name', nargs=1, help='base file name of main tex file (without .tex extension)')
     parser.add_argument('-v', '--verbose', action='store_true', help='verbose output')
+    parser.add_argument('-d', '--debug', action='store_true', help='debug output')
     parser.add_argument('-w', '--write', metavar="file_name", action='store', nargs='?', const=" ", type=str, help='Append output to .bib file (default: write output to stdout). The .bib file is automatically detected from the .blg file. If no .bib file is found, the .bib file has to be specified explicitly via "-w file_name.bib".')
 
     args = parser.parse_args()
@@ -49,41 +58,43 @@ def pybibparse():
         + re.findall(r'I couldn\'t open database file ([A-Za-z0-9.\-_\/]*)\n', blg_file)
 
     if missing_cites:
-        if args.write is None:
-            get_citations(missing_cites, verbose=args.verbose)
-        elif args.write != " ":
-            get_citations(missing_cites, verbose=args.verbose, file=args.write)
-        elif args.write == " " and bib_file_names:
-            get_citations(missing_cites, verbose=args.verbose, file=bib_file_names[0])
-        else:
-            print("No .bib file found. Please specify the .bib file via '-w file_name.bib'")
+        kwargs = {}
+        if args.debug:
+            kwargs['verbose'] = log.DEBUG
+        elif args.verbose:
+            kwargs['verbose'] = log.INFO
+        if args.write:
+            if args.write == " " and not bib_file_names:
+                print("No .bib file found. Please specify the .bib file via '-w file_name.bib'")
+                sys.exit()
+            kwargs['file'] = bib_file_names[0] if args.write == " " else args.write
+        asyncio.run(get_citations(missing_cites, **kwargs))        
     else:
-        print("No missing citations found. Make sure that biber/bibtex is run '\
-            'successfully before running pybibget.")
+        print("No missing citations found. Make sure that biber/bibtex is run successfully before running pybibget.")
 
 
-async def get_citations(keys, verbose=False, file=None):
+async def get_citations(keys, verbose=log.WARN, file=None):
     """
     Retrieves BibTeX entries for given citation keys and writes them to file or stdout
     """
-    bibentries = await asyncio.gather(*[bibentry.getbibentry(key, verbose=verbose) for key in keys])
-    #except ValueError as error:
-    #    print(error)
-    #    return
+    log.basicConfig(format="%(levelname)s: %(message)s", level=verbose)
+
+    bibentries = await asyncio.gather(*[bibentry.getbibentry(key) for key in keys],return_exceptions=True)
     bib_data = pybtex.database.BibliographyData()
-    for entry, key in bibentries:
-        if entry:
+    for entry_key in bibentries:
+        if type(entry_key) in [ValueError, TypeError]:
+            log.error(entry_key)
+        else:
+            entry,key = entry_key
             bib_data.entries[key] = entry
         
     number_of_entries = len(bib_data.entries)
     bib_data = bib_data.to_string('bibtex')
     if file:
-        with open(file, 'a') as file:
-            file.write(bib_data)
+        with open(file, 'a') as obj:
+            obj.write(bib_data)
             print(f"Successfully appended {number_of_entries} BibTeX entries to {file}.")
     else:
-        if verbose:
-            print("\n======================\n Output:\n======================")
         print("\n"+bib_data)
 
 
--- pybibget/bibentry.py
+++ pybibget/bibentry.py
@@ -1,5 +1,6 @@
 from urllib import parse
 import re
+import logging as log
 from lxml import html, etree
 import httpx
 from pybtex.database import Entry, Person, parse_string
@@ -9,12 +10,32 @@ ATOM = 'http://www.w3.org/2005/Atom'
 ARXIV = 'http://arxiv.org/schemas/atom'
 RE_MSC = r'MR\d{4,10}'
 RE_PMID = r'PMID:\d{4,10}'
-RE_DOI = r'10\.\d{4,9}\/[-._;()/:A-Za-z0-9]+'
+RE_DOI = r'10\.\d{4,9}\/[-._;()\/:A-Za-z0-9]+'
 RE_ARXIV_OLD = r'\b[a-zA-Z\-\.]{2,10}\/\d{7}(?:v\d)?\b'
 RE_ARXIV_NEW = r'\b\d{4}\.\d{4,5}(?:v\d)?\b'
 
 
-async def getbibentry(key, verbose=False):
+def msg_looking(key, service):
+    return f"Looking for {key} on {service}"
+
+
+def msg_found(key, service, continuation=None):
+    ret_str = f"{key} found on {service}"
+    if continuation:
+        ret_str += f". {continuation}"
+    return ret_str
+
+
+def msg_not_found(key, service, continuation=None, reason=None):
+    ret_str = f"{key} not found on {service}"
+    if reason:
+        ret_str += f" ({reason})"
+    if continuation:
+        ret_str += f". {continuation}"   
+    return ret_str
+
+
+async def getbibentry(key):
     """
     Get a bibentry from a citation key.
 
@@ -22,8 +43,6 @@ async def getbibentry(key, verbose=False):
     ----------
     key : str
         The citation key.
-    verbose : bool, optional
-        Whether to print the progress of the function. The default is False.
 
     Returns
     ---------
@@ -34,49 +53,37 @@ async def getbibentry(key, verbose=False):
     ValueError
         If the citation key is invalid or the entry is not found.
     """
-    result = None
-
     if re.match(RE_MSC, key):
-        if verbose:
-            print(f"Looking for {key} on MathSciNet...", end=" ")
-        try:
-            result = await get_mathscinet_bibentry(mrkey=key, verbose=verbose)
-        except ValueError as err:
-            print(err)
+        log.info(msg_looking(key, "MathSciNet"))
+        result = await get_mathscinet_bibentry(mrkey=key)
     elif re.match(RE_PMID, key):
-        if verbose:
-            print(f"Looking for {key} on PubMed...", end=" ")
-        result = await get_pubmed_bibentry(pmid=key, verbose=verbose)
+        log.info(msg_looking(key, "PubMed"))
+        result = await get_pubmed_bibentry(pmid=key)
     elif re.match(RE_ARXIV_OLD, key) or re.match(RE_ARXIV_NEW, key):
-        if verbose:
-            print(f"Looking for arXiv:{key}...", end=" ")
-        result = await get_arxiv_bibentry(key, verbose=verbose)
+        log.info(msg_looking(key, "arXiv"))
+        result = await get_arxiv_bibentry(key)
     elif re.match(RE_DOI, key):
         try:
-            if verbose:
-                print(f"Looking for {key} on MathSciNet...", end=" ")
-            result = await get_mathscinet_bibentry(doi=key, verbose=verbose)
-        except ValueError:
-            if verbose:
-                print(f"Not found. Looking for {key} on doi.org...", end=" ")
-            result = await get_doi_bibentry(key, verbose=verbose)
+            log.info(msg_looking(key, "MathSciNet"))
+            result = await get_mathscinet_bibentry(doi=key)
+        except Exception as exc:
+            log.info(exc)
+            result = await get_doi_bibentry(key)
     else:
         raise ValueError(f"Invalid citation key {key}!\n")
     return (result, key)
 
 
-async def get_mathscinet_bibentry(mrkey=None, doi=None, verbose=False):
+async def get_mathscinet_bibentry(mrkey=None, doi=None):
     """
     Get a bibentry from a MathSciNet citation key or DOI.
 
     Parameters
     ----------
     mrkey : str, optional
-        The MathSciNet citation key. The default is None.
+        The MathSciNet citation key, must start with MRxxx. The default is None.
     doi : str, optional
-        The DOI. The default is None.
-    verbose : bool, optional
-        Whether to print the progress of the function. The default is False.
+        The DOI, must start with 10.xxx/xxx. The default is None.
 
     Returns
     ---------
@@ -87,38 +94,36 @@ async def get_mathscinet_bibentry(mrkey=None, doi=None, verbose=False):
     ValueError
         If the neither mrnumber nor doi are provided, or the entry is not found.
     """
-    if mrkey:
-        url = "https://mathscinet.ams.org/mathscinet/search/publications.html?fmt=bibtex&pg1=MR&s1=" + mrkey[2:]
-    elif doi:
-        url = "https://mathscinet.ams.org/mathscinet/search/publications.html?fmt=bibtex&pg1=DOI&s1=" + doi
-    else:
-        raise ValueError("Either mrkey or doi must be specified.")
-    if verbose:
-        print(f"Connecting to {url}...", end=" ")
+    if not mrkey and not doi:
+        raise ValueError("Either MRnumber or doi must be specified.")
+    base_url = "https://mathscinet.ams.org/mathscinet/search/publications.html?fmt=bibtex&pg1="
+    url = base_url + "MR&s1=" + mrkey[2:] if mrkey else base_url + "DOI&s1=" + doi
     async with httpx.AsyncClient() as client:
         page = await client.get(url)
-        tree = html.fromstring(page.text)
-        if bibstrings := tree.xpath('//pre/text()'):
-            if verbose:
-                print("Success!")
+        try:
+            tree = html.fromstring(page.text)
+            bibstrings = tree.xpath('//pre/text()')
             bibstr = bibstrings[0]
-        else:
-            raise ValueError(f"{mrkey if mrkey else doi} not found! Please check the citation key and whether you have access to MathSciNet.")
-        entries = parse_string(bibstr, 'bibtex').entries
-        univ_id = list(entries.keys())[0]
-        return entries[univ_id]
+            if len(bibstr)>1:
+                log.warn(f"MathSciNet returned more than one entry for {mrkey if mrkey else doi}. Using the first one but this may be wrong.")
+            entries = parse_string(bibstr, 'bibtex').entries
+            log.info(msg_found(mrkey if mrkey else doi, "MathSciNet"))
+            return list(entries.values())[0]
+        except:
+            reason = str(page.status_code)
+            if page.status_code == 200:
+                reason += "; " + tree.xpath('//head/title/text()')[0].replace("\n", "")
+            raise ValueError(msg_not_found(mrkey if mrkey else doi, "MathSciNet", reason=reason, continuation="Trying crossref.org" if doi else None))
 
-
-async def get_doi_bibentry(doi, verbose=False):
+            
+async def get_doi_bibentry(doi):
     """
     Get a bibentry from a DOI.
 
     Parameters
     ----------
     doi : str
-        The DOI.
-    verbose : bool, optional
-        Whether to print the progress of the function. The default is False.
+        The DOI, must start with 10.xxx/xxx
 
     Returns
     ---------
@@ -129,29 +134,28 @@ async def get_doi_bibentry(doi, verbose=False):
     ValueError
         If the entry is not found.
     """
-    url = "https://doi.org/" + doi
+    url = "https://api.crossref.org/v1/works/" + doi + "/transform"
     headers = {'Accept': 'application/x-bibtex; charset=utf-8'}
     async with httpx.AsyncClient() as client:
         page = await client.get(url, headers=headers, follow_redirects=True)
-        if page.status_code == 200:
+        try:
             entries = parse_string(page.text, 'bibtex').entries
-            if verbose:
-                print("Success!")
-            return sanitize_entry(list(entries.values())[0])
-        else:
-            raise ValueError("Not found! Please check the citation key and whether you have access to doi.org.")
+            entry = sanitize_entry(list(entries.values())[0])
+            log.info(msg_found(doi, "crossref.org"))
+            return entry
+        except:
+            reason = page.status_code
+            raise ValueError(msg_not_found(doi, "crossref.org", reason=reason))
 
 
-async def get_arxiv_bibentry(arxiv_key, verbose=False):
+async def get_arxiv_bibentry(arxiv_key):
     """
     Get a bibentry from an arXiv identifier.
 
     Parameters
     ----------
     arxiv_key : str
-        The arXiv identifier.
-    verbose : bool, optional
-        Whether to print the progress of the function. The default is False.
+        The arXiv identifier. Can be either the old (hep-th/xxxxx) or the new (2201.xxxx) format.
 
     Returns
     ---------
@@ -165,40 +169,41 @@ async def get_arxiv_bibentry(arxiv_key, verbose=False):
     url = "http://export.arxiv.org/api/query?id_list=" + arxiv_key
     async with httpx.AsyncClient() as client:
         page = await client.get(url, follow_redirects=True)
-        tree = etree.fromstring(page.text.encode())
-        if doi := tree.xpath("//a:entry/b:doi", namespaces={'a': ATOM, 'b': ARXIV}):
-            if verbose:
-                print("Detected DOI in arXiv record...", end=" ")
-            bibentry, _ = await getbibentry(doi[0].text, verbose=verbose)
-        elif title := tree.xpath("//a:entry/a:title", namespaces={'a': ATOM}):
-            if verbose:
-                print("Success!")
-            fields = [("title", title[0].text)]
-            if journal := tree.xpath("//a:entry/a:journal", namespaces={'a': ATOM}):
-                fields += [("note", journal[0].text)]
+        try:
+            tree = etree.fromstring(page.text.encode())
+            if doi := tree.xpath("//a:entry/b:doi", namespaces={'a': ATOM, 'b': ARXIV}):
+                log.info(msg_found(arxiv_key, "arXiv", continuation=f"Detected {doi[0].text}"))
+                bibentry, _ = await getbibentry(doi[0].text)
+            elif title := tree.xpath("//a:entry/a:title", namespaces={'a': ATOM}):
+                fields = [("title", title[0].text)]
+                if journal := tree.xpath("//a:entry/a:journal", namespaces={'a': ATOM}):
+                    fields += [("note", journal[0].text)]
+                else:
+                    fields += [("note", "Preprint")]
+                fields += [("year", tree.xpath("//a:entry/a:published",namespaces={'a': ATOM})[0].text[:4])]
+                bibentry = Entry("unpublished", fields=fields)
+                bibentry.persons["author"] = [Person(author.text) for author in tree.xpath("//a:entry/a:author/a:name", namespaces={'a': ATOM})]
+                bibentry = sanitize_entry(bibentry)
+                log.info(msg_found(arxiv_key, "arXiv", continuation="No DOI found, using title and authors"))
             else:
-                fields += [("note", "Preprint")]
-            fields += [("year", tree.xpath("//a:entry/a:published",namespaces={'a': ATOM})[0].text[:4])]
-            bibentry = Entry("unpublished", fields=fields)
-            bibentry.persons["author"] = [Person(author.text) for author in tree.xpath("//a:entry/a:author/a:name", namespaces={'a': ATOM})]
-            bibentry = sanitize_entry(bibentry)
-        else:
-            raise ValueError(f"Could not find citation for {arxiv_key}. Please check the citation key and whether you have access to arXiv.")
-        bibentry.fields["eprint"] = arxiv_key
-        bibentry.fields["archiveprefix"] = "arXiv"
-        return bibentry
+                raise ValueError(f"empty arXiv entry returned")
+            bibentry.fields["eprint"] = arxiv_key
+            bibentry.fields["archiveprefix"] = "arXiv"
+            return bibentry
+        except Exception as exc:
+            reason = str(page.status_code) + "; " + exc.args[0]
+            raise ValueError(msg_not_found(arxiv_key, "arXiv", reason=reason))
+        
 
 
-async def get_pubmed_bibentry(pmid, verbose=False):
+async def get_pubmed_bibentry(pmid):
     """
     Get a bibentry from Pubmed for a given PubMed ID.
 
     Parameters
     ----------
     pmid : str
-        The PubMed ID. 
-    verbose : bool, optional
-        Whether to print the progress of the function. The default is False.
+        The PubMed ID, must start with "PMID:".
 
     Returns
     ---------
@@ -209,22 +214,22 @@ async def get_pubmed_bibentry(pmid, verbose=False):
     ValueError
         If the entry is not found.
     """
-    url = f'https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?ids={pmid[5:]}&idtype=pmid&format=json'
-    headers = {'Accept': 'application/json'}
+    url = f"https://pubmed.ncbi.nlm.nih.gov/{pmid[5:]}/?format=pubmed"
     async with httpx.AsyncClient() as client:
-        page = await client.get(url, headers=headers, follow_redirects=True)
-        if page.status_code == 200:
-            data = page.json()
-            try:
-                doi = data['records'][0]['doi']
-                if verbose:
-                    print(f"Detected doi.org/{doi} in PubMed record...", end=" ")
-                doi_entry, _ = await getbibentry(doi, verbose=verbose)
-                bibentry = sanitize_entry(doi_entry)
-            except Exception as exc:
-                raise ValueError(f'Could not find citation for {pmid}. Please check the citation key and whether you have access to PubMed.') from exc
+        page = await client.get(url, follow_redirects=True)
+        try:
+            doi = re.search("AID - (10\.\d{4,9}\/[-._;()\/:A-Za-z0-9]+) \[doi\]", page.text)
+            doi = doi.group(1)
+            log.info(msg_found(pmid, "PubMed", continuation=f"Detected {doi}"))
+            doi_entry, _ = await getbibentry(doi)
+            bibentry = sanitize_entry(doi_entry)
             bibentry.fields["PMID"] = pmid[5:]
             return bibentry
+        except Exception as exc:
+            reason = str(page.status_code)
+            if page.status_code == 200:
+                reason += "; " + data['records'][0]['errmsg']
+            raise ValueError(msg_not_found(pmid, "PubMed", reason=reason)) from exc
 
 
 def sanitize_entry(entry):
